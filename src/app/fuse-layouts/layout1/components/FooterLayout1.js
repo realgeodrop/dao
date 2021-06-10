69f17836273e5501881eab487ac3396e815315f7
Jeremie Olivier <jeremie.olivier.pro@gmail.com>
2021-06-10 04:42:40 +0200

    remove fuse bar

--- src/app/fuse-layouts/layout1/components/FooterLayout1.js
+++ src/app/fuse-layouts/layout1/components/FooterLayout1.js
@@ -14,12 +14,6 @@ function FooterLayout1(props) {
 
 	return (
 		<ThemeProvider theme={footerTheme}>
-			<AppBar
-				id="fuse-footer"
-				className={clsx('relative z-20 shadow-md', props.className)}
-				color="default"
-				style={{ backgroundColor: footerTheme.palette.background.paper }}
-			>
 				<Toolbar className="min-h-48 md:min-h-64 px-8 sm:px-12 py-0 flex items-center overflow-x-auto">
 					<div className="flex flex-grow flex-shrink-0">
 						<PurchaseButton className="mx-4" />
@@ -30,7 +24,6 @@ function FooterLayout1(props) {
 						<PoweredByLinks />
 					</div>
 				</Toolbar>
-			</AppBar>
 		</ThemeProvider>
 	);
 }
