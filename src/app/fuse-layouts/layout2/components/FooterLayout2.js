0f96e0b31352cec92ca4397070bcde660ac8a10e
Jeremie Olivier <jeremie.olivier.pro@gmail.com>
2021-06-10 04:51:23 +0200

    remove fuse banner

--- src/app/fuse-layouts/layout2/components/FooterLayout2.js
+++ src/app/fuse-layouts/layout2/components/FooterLayout2.js
@@ -14,23 +14,8 @@ function FooterLayout2(props) {
 
 	return (
 		<ThemeProvider theme={footerTheme}>
-			<AppBar
-				id="fuse-footer"
-				className={clsx('relative z-20 shadow-md', props.className)}
-				color="default"
-				style={{ backgroundColor: footerTheme.palette.background.paper }}
-			>
 				<Toolbar className="container min-h-48 md:min-h-64 px-8 sm:px-12 py-0 flex items-center overflow-x-auto">
-					<div className="flex flex-grow flex-shrink-0">
-						<PurchaseButton className="mx-4" />
-						<DocumentationButton className="mx-4" />
-					</div>
-
-					<div className="flex flex-grow flex-shrink-0 px-12 justify-end">
-						<PoweredByLinks />
-					</div>
 				</Toolbar>
-			</AppBar>
 		</ThemeProvider>
 	);
 }
